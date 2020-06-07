e49c2903915fc3c6f0166e6ce1d2fa7e0d39977c
Davilson Junior <davilsonjr_jobs@yahoo.com>
2020-06-07 01:03:17 -0300

    Application server

---
+++ server/src/routes.ts
@@ -0,0 +1,47 @@
+import express from 'express';
+import { celebrate, Joi } from 'celebrate';
+
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import ItemsController from './controllers/ItemsController';
+import PointsController from './controllers/PointsController';
+
+const routes = express.Router();
+const upload = multer(multerConfig);
+ 
+routes.get('/items', ItemsController.index);
+
+routes.get('/points', PointsController.index);
+routes.get('/points/:id', PointsController.show);
+
+routes.post(
+  '/points', 
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    }),
+  }, {
+    abortEarly: false,
+  }), 
+  PointsController.create
+);
+
+export default routes;
+
+
+/**
+ * index - Listar todos
+ * show - Lista um
+ * create - Criar um novo registro
+ * update - atualiza um registro
+ * delete - deleta um registro
+ */
